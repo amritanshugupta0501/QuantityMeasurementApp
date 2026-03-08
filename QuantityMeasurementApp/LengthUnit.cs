542034fa643950b9df0d23b23684b8a2e7da373d
Amritanshu Gupta <amritanshugupta0501@gmail.com>
2026-03-08 23:44:38 +0530

    [AmritanshuGupta].Add - Implemented Volume Measurement Functionalitites

--- QuantityMeasurementApp/LengthUnit.cs
+++ QuantityMeasurementApp/LengthUnit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace QuantityMeasurementApp
 {
+    // Enum for all the length units.
     public enum LengthUnit
     {
         INCH,
---
+++ QuantityMeasurementApp/VolumeUnit.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace QuantityMeasurementApp
+{
+    // Enum for all the volume units.
+    public enum VolumeUnit
+    {
+        LITRE,
+        MILLILITRE,
+        GALLON
+    }
+}
